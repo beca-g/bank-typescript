fb6a5c3ce54531af8a30e017d2647604095abb7f
Beca-g <rebekahgalliano@gmail.com>
2021-04-19 13:17:54 +0100

    Transactions are initialized with the date it was generated

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,9 +1,15 @@
 export class Transaction {
   type: string;
   amount: number;
+  date: number;
 
   constructor(type: string, amount: number) {
     this.type = type;
-    this.amount = amount
+    this.amount = amount;
+    this.date = Date.now();
+  }
+
+  getDate() {
+    return new Date(this.date).toLocaleString().split(',')[0];
   }
 }
--- tests/transaction.test.ts
+++ tests/transaction.test.ts
@@ -1,18 +1,26 @@
 import { Transaction } from "../src/transaction";
 import { expect } from "chai";
+// import { sinon } from "sinon";
+
 
 describe("Transaction", () => {
   let transaction: Transaction
-
+  
   beforeEach(function() {
-    transaction = new Transaction("deposit", 1000)
+    transaction = new Transaction("credit", 1000)
+    const date = 1618834297723
   })
 
+
   it("should store the transaction type", () => {
-    expect(transaction.type).to.equal("deposit")
+    expect(transaction.type).to.equal("credit")
   })
 
   it("should store the amount", () => {
     expect(transaction.amount).to.equal(1000)
+  })
+
+  it("should store the date of the transaction", () => {
+    expect(transaction.getDate()).to.equal("19/04/2021")
   })
 })
