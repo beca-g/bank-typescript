27e29e1516bf617084c062b1d0e984d5b7121b6f
Beca-g <rebekahgalliano@gmail.com>
2021-04-29 12:18:57 +0100

    Corrected eslint offences

--- tests/account.test.ts
+++ tests/account.test.ts
@@ -1,46 +1,47 @@
-import { Account } from "../src/account"
-import { expect } from "chai"
+import { Account } from "../src/account";
+import { expect } from "chai";
+// import mocha = require("mocha");
 
 describe("Account", () => {
-  let account: Account;
+  let account: Account = new Account();
   
   beforeEach(() => {
-    account = new Account()
-  })
+    account = new Account();
+  });
 
   describe("when initialized", () => {
     it("should be initialized with no transaction history", () => {
-      expect(account.transactions.length).to.equal(0)
-    })
-  })
+      expect(account.transactionList.length).to.equal(0);
+    });
+  });
 
   describe("balance", () => {
     it("should be initialized with a balance of zero", () => {
-      expect(account.balance()).to.equal(0)
-    })
-  })
+      expect(account.balance()).to.equal(0);
+    });
+  });
 
   describe("deposit", () => {
     it("should increase the account balance", () => {
-      account.deposit(1000)
-      expect(account.balance()).to.equal(1000)
-    })
+      account.deposit(1000);
+      expect(account.balance()).to.equal(1000);
+    });
 
     it("should add a transaction to the transactions array", () => {
-      account.deposit(1000)
-      expect(account.transactions.length).to.equal(1)
-    })
-  })
+      account.deposit(1000);
+      expect(account.transactionList.length).to.equal(1);
+    });
+  });
 
   describe("withdraw", () => {
     it("should decrease the account balance", () => {
-      account.deposit(1000)
-      account.withdraw(500)
-      expect(account.balance()).to.equal(500)
-    })
+      account.deposit(1000);
+      account.withdraw(500);
+      expect(account.balance()).to.equal(500);
+    });
 
     it("should throw an error if the account has insufficient funds", () => {
-      expect(() => account.withdraw(500)).to.throw("Insufficient funds")
-    })
-  })
-})
+      expect(() => account.withdraw(500)).to.throw("Insufficient funds");
+    });
+  });
+});
--- tests/transaction.test.ts
+++ tests/transaction.test.ts
@@ -2,24 +2,24 @@ import { Transaction } from "../src/transaction";
 import { expect } from "chai";
 
 describe("Transaction", () => {
-  let transaction: Transaction
-  let date = Date.now();
+  let transaction: Transaction;
+  const date = Date.now();
   let today: string;
   
   beforeEach(() => {
-    transaction = new Transaction("credit", 1000)
-    today = new Date(date).toLocaleString().split(',')[0];
-  })
+    transaction = new Transaction("credit", 1000);
+    today = new Date(date).toLocaleString().split(",")[0];
+  });
   
   it("should store the transaction type", () => {
-    expect(transaction.type).to.equal("credit")
-  })
+    expect(transaction.type).to.equal("credit");
+  });
   
   it("should store the amount", () => {
-    expect(transaction.amount).to.equal(1000)
-  })
+    expect(transaction.amount).to.equal(1000);
+  });
   
   it("should store the date of the transaction", () => {
-    expect(transaction.date).to.equal(today)
-  })
-})
+    expect(transaction.date).to.equal(today);
+  });
+});
